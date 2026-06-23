175657648561baeda5fdd2337508aef065f117a7
Grinenko Artur <grinenko.a@outlook.com>
2026-06-23 16:50:12 +0200

    Add files via upload

---
+++ vuetify3test/src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container class="fill-height d-flex flex-column justify-center ga-3" max-width="900">
+    <v-btn>Test no link</v-btn>
+    <v-btn href="#">Test with link</v-btn>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+</script>
