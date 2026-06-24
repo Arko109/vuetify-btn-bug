09060b93c5a67d6f6772bb673b15b46847eb8bc3
Artur Grinenko <grinenkoa@swatchgroup.net>
2026-06-24 11:14:43 +0200

    Git cli deserves gulag

---
+++ vuetify4norouter/src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container class="fill-height d-flex flex-row justify-center" max-width="1100">
+    <v-btn variant="text">Test no link</v-btn>
+    <v-btn href="#" variant="text">Test with link</v-btn>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+</script>
